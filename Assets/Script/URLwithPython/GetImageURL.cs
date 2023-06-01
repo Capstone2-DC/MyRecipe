57dc40b0a47e1f94e8ade2c79f1f2f5fa9953680
hyeonju0121 <125746081+zhun0922@users.noreply.github.com>
2023-06-02 08:08:46 +0900

    레시피 UI 구성

--- Assets/Script/URLwithPython/GetImageURL.cs
+++ Assets/Script/URLwithPython/GetImageURL.cs
@@ -5,23 +5,26 @@ using UnityEngine.UI;
 
 public class GetImageURL : MonoBehaviour
 {
-  /*  public static GetImageURL Insatnce { get; private set; }
-    private void Awake()
-    {
-        Insatnce = this;
-    }*/
+    /*  public static GetImageURL Insatnce { get; private set; }
+      private void Awake()
+      {
+          Insatnce = this;
+      }*/
 
+    public RawImage targetRawImage;
     public void CrawlingImage(string recipe)
     {
-        UnityEngine.Debug.Log(recipe);
+
         string pythonScriptPath = Application.dataPath + "/Script/URLwithPython/Python/RecipeSearch.py";
-        // string recipeName = "제육볶음";
         string recipeName = recipe;
 
         // Python 스크립트 실행
         ProcessStartInfo startInfo = new ProcessStartInfo();
         startInfo.FileName = "python";
-        startInfo.Arguments = $"{pythonScriptPath} \"{recipeName}\"";
+        //UnityEngine.Debug.Log("경로" + Path.Combine(Application.persistentDataPath, "recipe_image.jpg"));
+        UnityEngine.Debug.Log("경로" + Application.persistentDataPath + "/" + "recipe_image.jpg");
+        //startInfo.Arguments = $"{pythonScriptPath} \"{recipeName}\" \"{Path.Combine(Application.persistentDataPath, "recipe_image.jpg")}\"";
+        startInfo.Arguments = $"{pythonScriptPath} \"{recipeName}\" \"{Application.persistentDataPath + "/" + "recipe_image.jpg"}\"";
         startInfo.UseShellExecute = false;
         startInfo.RedirectStandardOutput = true;
 
@@ -35,6 +38,7 @@ public class GetImageURL : MonoBehaviour
 
         // 이미지 파일 로드
         string imagePath = Path.Combine(Application.persistentDataPath, "recipe_image.jpg");
+        //string imagePath = Application.persistentDataPath +"/"+ "recipe_image.jpg";
         UnityEngine.Debug.Log(imagePath);
         UnityEngine.Debug.Log(File.Exists(imagePath));
         if (File.Exists(imagePath))
@@ -45,13 +49,11 @@ public class GetImageURL : MonoBehaviour
             Texture2D texture = new Texture2D(1, 1);
             texture.LoadImage(imageBytes);
 
-            // 이미지를 원하는 곳에 표시하는 코드 작성
-            // 예시: RawImage 컴포넌트를 가진 GameObject에 텍스처를 할당하여 이미지를 표시
-            RawImage rawImage = GetComponent<RawImage>();
-            rawImage.texture = texture;
+            // Assign the texture to the targetRawImage
+            targetRawImage.texture = texture;
 
             // 이미지 파일 삭제
-           File.Delete(imagePath);
+            File.Delete(imagePath);
         }
         else
         {
